50012edb2c1db94851d0d0f50842996b78434302
Alfredo M <alf.mrt@gmail.com>
2020-03-07 02:30:16 +0100

    continua il tutorial

--- README.md
+++ README.md
@@ -2,18 +2,30 @@
 
 ## Tutorials
 
-
 Video: [https://www.youtube.com/watch?v=hnCmSXCZEpU](https://www.youtube.com/watch?v=hnCmSXCZEpU)  
 Scritto: [https://coursetro.com/posts/code/130/Learn-Bootstrap-4-Final-in-2018-with-our-Free-Crash-Course](https://coursetro.com/posts/code/130/Learn-Bootstrap-4-Final-in-2018-with-our-Free-Crash-Course)
 
-    npm init -y
-    npm -g install gulp-cli
-    npm i gulp browser-sync gulp-sass --save-dev
-            |__ oppure npm install **gulp@3.8.10** (per incompatibilìtà)
+```
+   npm init -y
+   npm -g install gulp-cli
+   npm i gulp browser-sync gulp-sass --save-dev
+           |__ oppure npm install **gulp@3.8.10** (per incompatibilìtà)
+```
+
+- `gulp` is a javascript task runner. You'll see how it works shortly if you're new.
+- `browser-sync` automatically refreshes our browser for us upon file changes.
+- `gulp-sass` enables sass compiling with our project.
+
+```
     npm i bootstrap jquery popper.js --save
+```
+
+- `bootstrap` of course is the bootstrap package.
+- `jquery` is used by bootstrap.
+- `popper.js` is also used by bootstrap. It allows for positioning of popovers, tooltips, etc..
 
 _Compilare scss_ con **npm**  
 https://webdesign.tutsplus.com/tutorials/watch-and-compile-sass-in-five-quick-steps--cms-28275
 
 Inizio from scratch di un react project con webpack e babel  
-https://www.valentinog.com/blog/babel/  
+https://www.valentinog.com/blog/babel/
--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,7 @@ var sass = require("gulp-sass");
 // Compile sass into CSS & auto-inject into browsers
 gulp.task("sass", function() {
   return gulp
-    .src([
-      "node_modules/bootstrap/scss/bootstrap.scss",
-      "src/assets/scss/*.scss"
-    ])
+    .src(["node_modules/bootstrap/scss/bootstrap.scss", "src/scss/*.scss"])
     .pipe(sass())
     .pipe(gulp.dest("src/css"))
     .pipe(browserSync.stream());
@@ -33,10 +30,25 @@ gulp.task("serve", ["sass"], function() {
   });
 
   gulp.watch(
-    ["node_modules/bootstrap/scss/bootstrap.scss", " src/assets/scss/*.scss"],
+    ["node_modules/bootstrap/scss/bootstrap.scss", " src/scss/*.scss"],
     ["sass"]
   );
   gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
 gulp.task("default", ["js", "serve"]);
+
+/*
+default task - When we type gulp in the command line, this is telling it to run both the js and serve tasks.
+js task      - This is simply specifying 3 different javascript files that are stored in the node_modules folder, 
+               which is created when we ran npm install ..., and moving them into our /src/js folder. 
+               This way, we're able to include them in our HTML file above by referencing /src/js instead of 
+               the node_modules folder.
+serve task   - The serve task launches a simple server and watches our sass files, and if any are changed, 
+               it calls the sass task. It also calls browser-sync when any * .html file is saved.
+sass task    - This takes both of the bootstrap sass files and our custom sass files and compiles them into 
+               regular CSS, and stores those CSS files into our /src/css folder.
+
+cfr. https://coursetro.com/posts/code/130/Learn-Bootstrap-4-Final-in-2018-with-our-Free-Crash-Course
+
+*/
